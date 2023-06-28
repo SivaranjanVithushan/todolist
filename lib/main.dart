9eb497267bd37740276a4c54b2cf0861472e9aab
Svithushan <ict19868@fot.sjp.ac.lk>
2023-06-28 17:32:06 +0530

    change the debugBanner Visible

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todolist/tasksscreen.dart';
+import 'package:todolist/screens/tasksscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: TasksScreen(),
     );
   }

b83e9827b6e279c201d7b01be187a1a479b89f77
Svithushan <ict19868@fot.sjp.ac.lk>
2023-06-30 08:49:58 +0530

    package possison change

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:todolist/models/task_data.dart';
+import 'package:provider/provider.dart';
 
 // ignore: must_be_immutable
 class AddTaskScreen extends StatelessWidget {
